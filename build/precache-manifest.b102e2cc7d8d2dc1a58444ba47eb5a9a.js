c56c8f8925dce8758880818947d3fac4df81b292
andrewcase <andrew.case801@gmail.com>
2020-03-26 19:34:15 -0400

    add build

---
+++ build/precache-manifest.b102e2cc7d8d2dc1a58444ba47eb5a9a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60b19fa129d78dbe3bcf0fb2e54aa8a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dbcb7e1ce10903dcdaf9",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "e69c4ae19862077dd3cf",
+    "url": "/static/css/main.7b420f60.chunk.css"
+  },
+  {
+    "revision": "dbcb7e1ce10903dcdaf9",
+    "url": "/static/js/2.05c827f6.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.05c827f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e69c4ae19862077dd3cf",
+    "url": "/static/js/main.1da8d368.chunk.js"
+  },
+  {
+    "revision": "44ddb9f3cf167541f1f9",
+    "url": "/static/js/runtime-main.6e1da91d.js"
+  }
+]);
