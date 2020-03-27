559ab46f52216d83cfa8af8009e2b905ef42abe0
andrewcase <andrew.case801@gmail.com>
2020-03-26 20:12:17 -0400

    Updates

---
+++ precache-manifest.91ee1e17f6923abf5e05672f28dee4a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2a0b213b23d202595c4c7bae0f4126f",
+    "url": "/musicBingo/index.html"
+  },
+  {
+    "revision": "dbcb7e1ce10903dcdaf9",
+    "url": "/musicBingo/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "f7f168f67b2ed97f992f",
+    "url": "/musicBingo/static/css/main.7b420f60.chunk.css"
+  },
+  {
+    "revision": "dbcb7e1ce10903dcdaf9",
+    "url": "/musicBingo/static/js/2.05c827f6.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/musicBingo/static/js/2.05c827f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7f168f67b2ed97f992f",
+    "url": "/musicBingo/static/js/main.30626699.chunk.js"
+  },
+  {
+    "revision": "78ec5cfed95266fa033a",
+    "url": "/musicBingo/static/js/runtime-main.783d4162.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e085778d2ccca1a8f0334240623ed25f.js"
+  "/musicBingo/precache-manifest.91ee1e17f6923abf5e05672f28dee4a0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/musicBingo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
